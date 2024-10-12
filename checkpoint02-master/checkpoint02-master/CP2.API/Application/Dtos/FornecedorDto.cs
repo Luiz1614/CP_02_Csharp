57020a0cd49ed169ef43df24efbb1b40351ef01b
Luiz Otávio <luizotaviosilva492@gmail.com>
2024-10-12 18:36:39 -0300

    Implementando DTOs

--- checkpoint02-master/checkpoint02-master/CP2.API/Application/Dtos/FornecedorDto.cs
+++ checkpoint02-master/checkpoint02-master/CP2.API/Application/Dtos/FornecedorDto.cs
@@ -1,6 +1,25 @@
-﻿namespace CP2.API.Application.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CP2.API.Application.Dtos
 {
     public class FornecedorDto
     {
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O CNPJ é obrigatório.")]
+        [RegularExpression(@"\d{14}", ErrorMessage = "O CNPJ deve conter 14 dígitos.")]
+        public string CNPJ { get; set; }
+
+        [Required(ErrorMessage = "O endereço é obrigatório.")]
+        public string Endereco { get; set; }
+
+        [Required(ErrorMessage = "O telefone é obrigatório.")]
+        [Phone(ErrorMessage = "O telefone não é válido.")]
+        public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "O email é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O email não é válido.")]
+        public string Email { get; set; }
     }
 }
--- checkpoint02-master/checkpoint02-master/CP2.API/Application/Dtos/VendedorDto.cs
+++ checkpoint02-master/checkpoint02-master/CP2.API/Application/Dtos/VendedorDto.cs
@@ -1,6 +1,37 @@
-﻿namespace CP2.API.Application.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CP2.API.Application.Dtos
 {
     public class VendedorDto
     {
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O email é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O email não é válido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O telefone é obrigatório.")]
+        [Phone(ErrorMessage = "O telefone não é válido.")]
+        public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "A data de nascimento é obrigatória.")]
+        public DateTime DataNascimento { get; set; }
+
+        [Required(ErrorMessage = "O endereço é obrigatório.")]
+        public string Endereco { get; set; }
+
+        [Required(ErrorMessage = "A data de contratação é obrigatória.")]
+        public DateTime DataContratacao { get; set; }
+
+        [Required(ErrorMessage = "O percentual de comissão é obrigatório.")]
+        [Range(0, 100, ErrorMessage = "O percentual de comissão deve estar entre 0 e 100.")]
+        public decimal ComissaoPercentual { get; set; }
+
+        [Required(ErrorMessage = "A meta mensal é obrigatória.")]
+        public decimal MetaMensal { get; set; }
+
+        [Required(ErrorMessage = "A data de criação é obrigatória.")]
+        public DateTime CriadoEm { get; set; }
     }
 }
